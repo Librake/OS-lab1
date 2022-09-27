45dcc9d79a50d1f43f8d1ce6ab118aed80cff517
Librake <ya.snake54@ya.ru>
2022-09-27 17:39:40 +0300

    refactor: script6

--- script6.sh
+++ script6.sh
@@ -1,44 +1,31 @@
 #! /bin/bash
 
+function ChangeAndWriteToFile 
+{
+    for line in `cat $inFile`
+    do
+        IFS=$' '
+        wordCount=1
+        for word in $line
+        do
+            if [ $wordCount -eq 3 ]
+            then
+                if [ $word = $1 ]
+                then
+                    echo ${line/$1/$2} >> $outFile
+                fi
+                break
+            fi
+            ((wordCount++))
+        done
+        IFS=$'\n'
+    done
+}
+
 IFS=$'\n'
 inFile="/var/log/anaconda/X.log"
 outFile="full.log"
 echo > $outFile
 
-for line in `cat $inFile`
-do
-    IFS=$' '
-    wordCount=1
-    for word in $line
-    do
-        if [ $wordCount -eq 3 ]
-        then
-            if [ $word = '(WW)' ]
-            then
-                echo ${line/'(WW)'/'Warning:'} >> $outFile
-            fi
-            break
-        fi
-        ((wordCount++))
-    done
-    IFS=$'\n'
-done
-
-for line in `cat $inFile`
-do
-    IFS=$' '
-    wordCount=1
-    for word in $line
-    do
-        if [ $wordCount -eq 3 ]
-        then
-            if [ $word = '(II)' ]
-            then
-                echo ${line/'(II)'/'Information:'} >> $outFile
-            fi
-            break
-        fi
-        ((wordCount++))
-    done
-    IFS=$'\n'
-done
+ChangeAndWriteToFile '(WW)' 'Warning:'
+ChangeAndWriteToFile '(II)' 'Information:'
